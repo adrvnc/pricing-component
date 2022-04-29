568e96bfe4af28fc28ff29804fd4158c9b0ac67d
Adrian Carter <adriancodesgit@gmail.com>
2022-04-28 21:22:25 -0500

    added webpage screenshot and modified JavaScript code

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function priceChange() {
-  let checkBox = document.getElementById("toggleSwitch");
+  let toggleSwitch = document.getElementById("toggleSwitch");
   let price1 = document.getElementsByClassName("price1");
   let price2 = document.getElementsByClassName("price2");
 
   for (let i = 0; i < price1.length; i++) {
-    if (checkBox.checked == true) {
+    if (toggleSwitch.checked == true) {
       price1[i].style.display = "block";
       price2[i].style.display = "none";
-    } else if (checkBox.checked == false) {
+    } else if (toggleSwitch.checked == false) {
       price1[i].style.display = "none";
       price2[i].style.display = "block";
     }
